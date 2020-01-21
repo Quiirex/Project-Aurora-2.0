85f9ad1fca6cf39b0ce8c02aea8b973f7d973828
Quiirex <58704360+Quiirex@users.noreply.github.com>
2020-01-21 15:21:56 +0100

    Add files via upload

---
+++ Commands.cs
@@ -0,0 +1,47 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Aurora_2._0
+{
+    public class Commands : ModuleBase
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            var msg = await ReplyAsync("***Calculating***");
+
+            await ReplyAsync($"***{(DateTime.Now - msg.Timestamp.LocalDateTime).TotalMilliseconds}*** ms.");
+
+            //await Context.Message.DeleteAsync();
+            await msg.DeleteAsync();
+        }
+        [Command("kick")]
+        public async Task Kick(SocketGuildUser mention, string reason = null)
+        {
+            if (mention != null)
+            {
+                var channel = await mention.GetOrCreateDMChannelAsync();
+                await channel.SendMessageAsync(reason == null ? $"Kicked from server {Context.Guild.Name} for an unspecified reason." 
+                    : $"Kicked from server {Context.Guild.Name} for ***{reason}***");
+                await Task.Delay(2000);
+                await mention.KickAsync();
+                await ReplyAsync(reason == null ? $"Kicked { mention.Username} for an unspecified reason."
+                    : $"Kicked {mention.Username} for {reason}");
+            }
+            else
+            {
+                await ReplyAsync("Specify the user first!");
+            }
+        }
+        [Command("skrivnostvere")]
+        public async Task SkrivnostVere()
+        {
+            await ReplyAsync($"Tvojo smrt oznanjamo, Gospod, in tvoje vstajenje slavimo, dokler ne prideš v slavi.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Aurora_2._0
+{
+    class Program
+    {
+        public DiscordSocketClient Client;
+        public CommandHandler Handler;
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        public async Task Start()
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+            Client.Log += Log;
+
+            Handler = new CommandHandler();
+
+            await Client.LoginAsync(Discord.TokenType.Bot, "MjczMTM1OTc0NjA5ODQ2Mjcy.Xibuzg.jVJL-OH0gSQ9oumQ0AsxZqEcDOc", true);
+
+            await Client.StartAsync();
+
+            await Handler.Install(Client);
+
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
